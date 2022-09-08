95666a580f0041274f2e546c3e2cc17e8aa6c24d
Mohamed Yasser <myasser@youxel.com>
2022-09-08 12:32:54 +0200

    Application Program

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using System.Net;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("{userName}",Name ="GetOrder")]
+        [ProducesResponseType(typeof(IEnumerable<OrdersVM>),(int)HttpStatusCode.OK )]
+        public async Task<ActionResult<IEnumerable<OrdersVM>>> GetOrdersByUserName (string userName)
+        {
+            var query = new GetOrderListQuery(userName);
+            var orders = _mediator.Send(query);
+            return Ok(orders);
+        }
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand model)
+        {
+            var results = await _mediator.Send(model);
+            return Ok(results);
+        }
+        [HttpPost("UpdateOrder")]
+        [ProducesResponseType((int) HttpStatusCode.NoContent)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand model)
+        {
+            await _mediator.Send(model);
+            return NoContent();
+        }
+        [HttpDelete("{id}" , Name ="DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var command = new DeleteOrderCommand() { Id = id};
+            await _mediator.Send(command);
+            return NoContent();
+        }
+       
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext :DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options):base(options)
+        {
+        }
+       public DbSet<Order> Orders { set; get; }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (item.State)
+                {
+                    case EntityState.Modified:
+                        item.Entity.LastModifiedBy = "svn";
+                        item.Entity.LastModifiedDate = DateTime.Now;    
+                        break;
+                    case EntityState.Added:
+                        item.Entity.CreatedBy = "svn";
+                        item.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
